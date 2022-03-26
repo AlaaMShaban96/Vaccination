3311e406e6eebe8cd84630a64067ac944a2ebf10
Alaa M Shaban <ala96ala96@gmail.com>
2022-03-26 16:21:00 +0200

    add icone

--- resources/views/VaccinationCenters/master.blade.php
+++ resources/views/VaccinationCenters/master.blade.php
@@ -8,9 +8,11 @@
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link href='https://fonts.googleapis.com/css?family=Tajawal' rel='stylesheet'>
+<script src="https://kit.fontawesome.com/5366d31a4a.js" crossorigin="anonymous"></script>
+
 @yield('head')
 <link rel="stylesheet" href="{{asset('css/master.css')}}">
 <style>
